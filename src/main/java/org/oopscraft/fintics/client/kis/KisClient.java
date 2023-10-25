814feb7dea88f70d177c4935aca84d0d24e2efd3
chomookun <chomookun@gmail.com>
2023-10-25 23:49:51 +0900

    .

--- src/main/java/org/oopscraft/fintics/client/kis/KisClient.java
+++ src/main/java/org/oopscraft/fintics/client/kis/KisClient.java
@@ -147,7 +147,7 @@ public class KisClient extends Client {
         LocalTime closeTime = LocalTime.of(15,30);
         LocalTime fidInputHour1Time = (nowTime.isAfter(closeTime) ? closeTime : nowTime);
 
-        for(int i = 0; i < 4; i ++) {
+        for(int i = 0; i < 6; i ++) {
             String url = apiUrl + "/uapi/domestic-stock/v1/quotations/inquire-time-itemchartprice";
             HttpHeaders headers = createHeaders();
             headers.add("tr_id", "FHKST03010200");
