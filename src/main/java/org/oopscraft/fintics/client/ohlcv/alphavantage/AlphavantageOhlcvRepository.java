7e22edf996630c72048dfaa7043cf7d1502b9eef
chomookun <chomookun@gmail.com>
2024-05-08 21:59:37 +0900

    remove alpha vantage ohlcv client

--- src/main/java/org/oopscraft/fintics/client/ohlcv/alphavantage/AlphavantageOhlcvRepository.java
+++
@@ -1,9 +0,0 @@
-package org.oopscraft.fintics.client.ohlcv.alphavantage;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AlphavantageOhlcvRepository extends JpaRepository<AlphavantageOhlcvEntity, AlphavantageOhlcvEntity.Pk> {
-
-}
