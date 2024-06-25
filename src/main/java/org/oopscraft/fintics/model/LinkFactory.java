9c0bbf2e8370000a0238c60e2a97f1565bcd50a8
chomookun <chomookun@gmail.com>
2024-06-25 23:09:05 +0900

    change link page chat to main

--- src/main/java/org/oopscraft/fintics/model/LinkFactory.java
+++ src/main/java/org/oopscraft/fintics/model/LinkFactory.java
@@ -12,11 +12,11 @@ public class LinkFactory {
         List<Link> links = new ArrayList<>();
         switch(Optional.ofNullable(market).orElse("")) {
             case "US" -> {
-                links.add(Link.of("Yahoo", "https://finance.yahoo.com/chart/" + symbol));
+                links.add(Link.of("Yahoo", "https://finance.yahoo.com/quote/" + symbol));
                 links.add(Link.of("Finviz", "https://finviz.com/quote.ashx?t=" + symbol));
             }
             case "KR" -> {
-                links.add(Link.of("Naver", "https://finance.naver.com/item/fchart.naver?code=" + symbol));
+                links.add(Link.of("Naver", "https://finance.naver.com/item/main.naver?code=" + symbol));
                 links.add(Link.of("Alphasquare", "https://alphasquare.co.kr/home/market-summary?code=" + symbol));
             }
             case "UPBIT" -> {
