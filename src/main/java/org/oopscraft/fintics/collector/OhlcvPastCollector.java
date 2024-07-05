2747f6e17041f080fc62af741de30fdc0d14412e
chomookun <chomookun@gmail.com>
2024-07-05 11:55:22 +0900

    disable ohlcv past collector temp

--- src/main/java/org/oopscraft/fintics/collector/OhlcvPastCollector.java
+++ src/main/java/org/oopscraft/fintics/collector/OhlcvPastCollector.java
@@ -23,7 +23,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-@Component
+//@Component
 @RequiredArgsConstructor
 @Slf4j
 public class OhlcvPastCollector extends AbstractCollector {
