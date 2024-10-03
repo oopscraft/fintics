dae44d2e768af9e1aec53626e6d4b6fd29e5d413
chomookun <chomookun@gmail.com>
2024-10-03 13:37:21 +0900

    profit - adds period search condition. " "

--- src/main/java/org/oopscraft/fintics/api/v1/ProfitsRestController.java
+++ src/main/java/org/oopscraft/fintics/api/v1/ProfitsRestController.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.oopscraft.fintics.api.v1.dto.ProfitResponse;
 import org.oopscraft.fintics.model.Profit;
 import org.oopscraft.fintics.service.ProfitService;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -28,10 +29,14 @@ public class ProfitsRestController {
      */
     @GetMapping("{brokerId}")
     @Operation(description = "gets realized profits")
-    public ResponseEntity<ProfitResponse> getRealizedProfits(
+    public ResponseEntity<ProfitResponse> getProfits(
             @PathVariable("brokerId") String brokerId,
-            @RequestParam(value = "dateFrom", required = false) LocalDate dateFrom,
-            @RequestParam(value = "dateTo", required = false) LocalDate dateTo
+            @RequestParam(value = "dateFrom", required = false)
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+                    LocalDate dateFrom,
+            @RequestParam(value = "dateTo", required = false)
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+                    LocalDate dateTo
     ) {
         Profit realizedProfit = realizedProfitService.getProfit(brokerId, dateFrom, dateTo);
         ProfitResponse realizedProfitResponse = ProfitResponse.from(realizedProfit);
